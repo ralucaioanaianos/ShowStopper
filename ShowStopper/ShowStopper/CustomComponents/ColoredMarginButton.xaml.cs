d06ccf689c664456a5dcf989a05a4b5e8bd9a209
ralucaioanaianos <raluca_ianos@yahoo.com>
2023-05-07 16:57:42 +0300

    made the background of the button gradient

--- ShowStopper/ShowStopper/CustomComponents/ColoredMarginButton.xaml.cs
+++ ShowStopper/ShowStopper/CustomComponents/ColoredMarginButton.xaml.cs
@@ -15,13 +15,31 @@ public partial class ColoredMarginButton
 	}
 
     private static readonly BindableProperty ButtonBackgroundColorProperty
-            = BindableProperty.Create(nameof(ButtonBackgroundColor), typeof(Color),
+            = BindableProperty.Create(nameof(ButtonBackgroundColor), typeof(GradientBrush),
                 typeof(ColoredMarginButton));
 
-    public Color ButtonBackgroundColor
+    public GradientBrush ButtonBackgroundColor
     {
-        get => (Color)GetValue(ButtonBackgroundColorProperty);
+        get => (GradientBrush)GetValue(ButtonBackgroundColorProperty);
         set => SetValue(ButtonBackgroundColorProperty, value);
+    }
+
+    private static readonly BindableProperty GradientColor1Property
+           = BindableProperty.Create(nameof(GradientColor1), typeof(Color), typeof(ColoredMarginButton));
+
+    public Color GradientColor1
+    {
+        get => (Color)GetValue(GradientColor1Property);
+        set => SetValue(GradientColor1Property, value);
+    }
+
+    private static readonly BindableProperty GradientColor2Property
+           = BindableProperty.Create(nameof(GradientColor2), typeof(Color), typeof(ColoredMarginButton));
+
+    public Color GradientColor2
+    {
+        get => (Color)GetValue(GradientColor2Property);
+        set => SetValue(GradientColor2Property, value);
     }
 
     private static readonly BindableProperty TextColorProperty
