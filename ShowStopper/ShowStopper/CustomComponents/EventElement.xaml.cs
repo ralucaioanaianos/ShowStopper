6b86425136342341d5c8e1724852a4c1c7f62bf5
Raluca Ianos <raluca_ianos@yahoo.com>
2023-06-06 21:34:55 +0300

    created custom component for event

---
+++ ShowStopper/ShowStopper/CustomComponents/EventElement.xaml.cs
@@ -0,0 +1,11 @@
+namespace ShowStopper.CustomComponents;
+
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class EventElement : ContentView
+{
+	public EventElement()
+	{
+		InitializeComponent();
+	}
+}
